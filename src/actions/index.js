8952d67fae058c562526896ffb424b1b9b486f41
HollyJHuber <hollyjhuber@gmail.com>
2017-11-10 06:07:23 -0500

    add reducers/shuffle.js and actions/index.js

---
+++ src/actions/index.js
@@ -0,0 +1,25 @@
+// export function shuffleCards() {
+//     return {
+//         type: "SHUFFLE_CARDS"
+//     }
+// }
+// see shuffle.js
+
+// export function flipCard(card) {
+//     return {
+//         type: "FLIP_CARD",
+//         card
+//     }
+// }
+
+// export function nextCard() {
+//     return {
+//         type: "NEXT_CARD"
+//     }
+// }
+
+// export function prevCard() {
+//     return {
+//         type: "PREV_CARD"
+//     }
+// }
---
+++ src/reducers/shuffle.js
@@ -0,0 +1,40 @@
+const cardsOriginal = [
+    {
+        id: 100,
+        question: "What is code?",
+        answer: "code is the enemy"
+    },
+    {
+        id: 101,
+        question: "What is state?",
+        answer: "state is the truth; it is always there. It is the singularity."
+    },
+    {
+        id: 102,
+        question: "What is true?",
+        answer: "We have our truth and we are living it out in our lives."
+    },
+    {
+        id: 103,
+        question: "What is the circle of life?",
+        answer: "HTML --> handler --> State --> render() --> HTML"
+    },    
+    {
+        id: 104,
+        question: "What will happen if you push to master?",
+        answer: "Ross will fire you."
+    },
+    {
+        id: 105,
+        question: "What is a good way to live your life?",
+        answer: "Yes, and ..."
+    },
+];
+
+
+/* make a copy of the array and sort it randomly using arrow function
+that copy will be used to generate the card content
+use id as key
+use index +1 as the cardNumber
+use array.length to determine the last card
+*/
